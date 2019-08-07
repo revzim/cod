9d883e4eadfec93615242ca1371005063d48de84
andy <andyzim11@gmail.com>
2019-08-07 17:48:28 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# cod (custom object detection)
+
+##
+
+#### [Tensorflow](https://www.tensorflow.org/) and transfer learning to train a custom model for object detection.
+
+##
+
+##### Environment: ` Windows 10 PC Home Edition | NVIDIA GeForce GTX 1060 6GB `
+
+##
+
+##### Before attempting custom object detection, I checked out the tutorial for [Tensorflow Image Classification](https://www.tensorflow.org/hub/tutorials/image_retraining) and successfully tested certain classification for different images. I used the images provided within that tutorial for this project to classify certain species of flowers because why not. A simplified description of my process is outlined below.
+
+##
+
+#### Process:
+` * CONSIDER OBJECT(S) FOR DETECTION `
+` * GATHER DATA AND OBJECTS `
+` * LABEL DATA AND OBJECTS WITH IMAGE LABEL SOFTWARE ` (I used [labelImg](https://github.com/tzutalin/labelImg) )
+` * GENERATE LABEL MAP (label_map.pbtxt) FOR EACH OBJECT CLASS `
+` * labelImg STORED AND SAVED DATA AS XML WILL NEED TO BE CONVERTED TO CSV IN ORDER TO BE CONVERTED LATER TO TFRECORDS `
+` * ONCE CONVERTED, THE CSV LABELED IMAGE FILES WILL BE USED TO GENERATE TFRECORDS FOR OUR THE TRAINING MODEL `
+` * GENERATE TFRECORDS FROM LABELED CSV DATA (gen_tf_records.py) `
+` * TFRECORDS ARE GENERATED INTO TWO SEPARATE ENTITIES: TRAINING AND TESTING `
+` * CREATE OR MODIFY AN EXISTING TRAINING CONFIG (training/training.config) FOR OUR CUSTOM OBJECT DETECTION `
+` * TRAIN MODEL (train.py) (this can take hours or days depending on your data and model and/or hyperparams) `
+` * ONCE SATISFIED, OR TIRED OF WAITING, AN INFERENCE GRAPH, INCLUDING ANY MODEL CHECKPOINT DATA, MUST BE CREATED IN ORDER TO TEST THE CUSTOM-TRAINED MODEL `
+` * TENSORFLOW PROVIDES A SCRIPT TO GENERATE THE INFERENCE GRAPH (export_inference_graph.py), WHICH MUST BE DONE IN ORDER TO TEST OUR CUSTOM-TRAINED MODEL `
+` * ONCE COMPLETED, THE GENERATED INFERENCE GRAPH CAN NOW BE USED FOR TESTING OUR CUSTOM-TRAINED MODEL `
+` * TESTING USES Tensorflow's Object Detection API, I HAVE PROVIDED MY TESTING FILE WITH THIS REPO (test_cod.py) `
+` * THE PROVIDED TEST FILE (test_cod.py) WILL TEST OUR NEWLY CREATED CUSTOM-TRAINED MODEL AGAINST THE PROVIDED TEST IMAGE AND OUTPUT THE RESULTS WITH BOXES AND SCORES AROUND ANY OBJECTS THE MODEL DETECTS `
+` * I HAVE PROVIDED IMAGES WITHIN (testers/) AS AN EXAMPLE OF THE MODELS LEARNING OVER TIME 2001 STEPS -> 20004 STEPS `
+` * OLDER INFERENCE GRAPHS ARE PROVIDED, HOWEVER, OBVIOUSLY, THEY WILL YIELD WORSE RESULTS THAN THE CURRENT (inference_graph/) DIRECTORY `
+
+##
+
+** * This repository should be used for learning purposes. My custom-trained model that is provided within this repository has been trained for ~20000 steps. ** 
+
+** * Note: The training process according to the config file (training/training.config) limits the training process to 200000 steps, which the authors at Tensorflow found to be "sufficient enough to train the... dataset." This amount of steps will effectively bypass the learning rate schedule, leading to a never-decaying learning rate. **
+
+** * Any implementation or use of Tensorflow requires Tensorflow, along with other libraries and API's to be installed. Installation instructions for Tensorflow can be found [here](https://www.tensorflow.org/install). **
+
+** * Certain files and directories within this repository were moved here for viewing purposes. Thus, linking and certain files might yield warnings/errors due to the nature of this repository and the nature of Tensorflow. **
+
+##
+
+* [Tensorflow: Research](https://github.com/tensorflow/models/tree/master/research/object_detection)
+* [Tensorflow: Object Detection API](https://github.com/tensorflow/models/tree/master/research/object_detection)
+* [Tensorflow: Training Custom Object Detector](https://tensorflow-object-detection-api-tutorial.readthedocs.io/en/latest/training.html)
+* [labelImg](https://github.com/tzutalin/labelImg)
+* [Datitran Dataset](https://github.com/datitran/raccoon_dataset)
+* [Tensorflow: Model Zoo for Transfer Learning & Hubs](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md)
---
+++ test_cod.py
@@ -0,0 +1,211 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from distutils.version import StrictVersion
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+# from absl import flags
+
+import numpy as np
+from PIL import Image
+from PIL import ImageColor
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageOps
+
+# tf flags required
+flags = tf.app.flags
+
+flags.DEFINE_integer(
+    'iter_val', None, 'Test round '
+    'Test round for output image value.')
+
+# object_detection folder
+sys.path.append("..")
+from object_detection.utils import ops as utils_ops
+
+if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
+  raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
+
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
+
+FLAGS = flags.FLAGS
+
+MODEL_NAME = 'H:/Tensorflow/custom_models/object_detection/custom_detection/inference_graph'
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_LABELS = 'H:/Tensorflow/custom_models/object_detection/custom_detection/training/label_map.pbtxt'
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+def resize_image(file_path, new_width=256, new_height=256,
+                              display=False):
+  pil_image = Image.open(file_path)
+  pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
+  pil_image_rgb = pil_image.convert("RGB")
+  pil_image_rgb.save(file_path, format="JPEG", quality=90)
+  return pil_image_rgb
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+PATH_TO_TEST_IMAGES_DIR = 'H:/Tensorflow/custom_models/object_detection/custom_detection/testers'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(5,6) ]
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+# run_inference_for_single_image(Image.open("H:/Tensorflow/custom_models/image_classifier/flower_photos/dandelion/16237158409_01913cf918_n.jpg"), od_graph_def)
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+      if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[1], image.shape[2])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: image})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.int64)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+  return output_dict
+
+def save_image(img, head_dir, img_num):
+  img = Image.fromarray(img)
+  img.save("H:/Tensorflow/custom_models/object_detection/custom_detection/{}/test{}.png".format(head_dir, img_num))
+
+def draw_bounding_box_on_image(image,
+                               ymin,
+                               xmin,
+                               ymax,
+                               xmax,
+                               color,
+                               font,
+                               thickness=3,
+                               display_str_list=()):
+  """Adds a bounding box to an image."""
+  draw = ImageDraw.Draw(image)
+  im_width, im_height = image.size
+  (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                ymin * im_height, ymax * im_height)
+  draw.line([(left, top), (left, bottom), (right, bottom), (right, top),
+             (left, top)],
+            width=thickness,
+            fill=color)
+
+  # If the total height of the display strings added to the top of the bounding
+  # box exceeds the top of the image, stack the strings below the bounding box
+  # instead of above.
+  display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+  # Each display_str has a top and bottom margin of 0.05x.
+  total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+  if top > total_display_str_height:
+    text_bottom = top
+  else:
+    text_bottom = bottom + total_display_str_height
+  # Reverse list and print from bottom to top.
+  for display_str in display_str_list[::-1]:
+    text_width, text_height = font.getsize(display_str)
+    margin = np.ceil(0.05 * text_height)
+    draw.rectangle([(left, text_bottom - text_height - 2 * margin),
+                    (left + text_width, text_bottom)],
+                   fill=color)
+    draw.text((left + margin, text_bottom - text_height - margin),
+              display_str,
+              fill="black",
+              font=font)
+    text_bottom -= text_height - 2 * margin
+
+def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.2):
+  """Overlay labeled boxes on an image with formatted scores and label names."""
+  colors = list(ImageColor.colormap.values())
+
+  font = ImageFont.load_default()
+  for i in range(min(boxes.shape[0], max_boxes)):
+    if scores[i] >= min_score:
+      ymin, xmin, ymax, xmax = tuple(boxes[i].tolist())
+      display_str = "{}: {}%".format(class_names[i],
+                                     int(100 * scores[i]))
+      color = colors[hash(class_names[i]) % len(colors)]
+      image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+      draw_bounding_box_on_image(
+          image_pil,
+          ymin,
+          xmin,
+          ymax,
+          xmax,
+          color,
+          font,
+          display_str_list=[display_str])
+      np.copyto(image, np.array(image_pil))
+  return image
+
+i = 1
+last_iter = []
+for image_path in TEST_IMAGE_PATHS:
+  image = resize_image(image_path)
+  # the array based representation of the image will be used later in order to prepare the
+  # result image with boxes and labels on it.
+  image_np = load_image_into_numpy_array(image)
+  # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+  image_np_expanded = np.expand_dims(image_np, axis=0)
+  # detection
+  output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
+  # DRAW IMAGE WITH BOXES AND SAVE
+  image_with_boxes = draw_boxes(
+    image_np, output_dict["detection_boxes"],
+    output_dict["detection_classes"], output_dict["detection_scores"])
+  save_image(image_with_boxes, "testers/output", i + FLAGS.iter_val)
+  i += 1
+
